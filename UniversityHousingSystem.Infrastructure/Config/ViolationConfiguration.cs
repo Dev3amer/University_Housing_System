42aa228229f35d91df2b34cd7a9e8a3a59cff29b
Dev3amer <mohamedamer8921@gmail.com>
2025-02-24 05:39:48 +0200

    Add Configurations for Violation, and ViolationType models

---
+++ UniversityHousingSystem.Infrastructure/Config/ViolationConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UniversityHousingSystem.Data.Entities;
+
+namespace UniversityHousingSystem.Infrastructure.Config
+{
+    public class ViolationConfiguration : IEntityTypeConfiguration<Violation>
+    {
+        public void Configure(EntityTypeBuilder<Violation> builder)
+        {
+            builder.HasKey(v => v.ViolationId);
+
+            // Properties
+            builder.Property(v => v.ViolationDate)
+                .IsRequired()
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            // Relationships
+            builder.HasOne(v => v.ViolationType)
+                .WithMany(vt => vt.Violations)
+                .HasForeignKey(v => v.ViolationTypeId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(v => v.StudentHistory)
+                .WithMany(sh => sh.Violations)
+                .HasForeignKey(v => v.StudentHistoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.ToTable("Violations");
+        }
+    }
+}
---
+++ UniversityHousingSystem.Infrastructure/Config/ViolationTypeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UniversityHousingSystem.Data.Entities;
+
+namespace UniversityHousingSystem.Infrastructure.Config
+{
+    public class ViolationTypeConfiguration : IEntityTypeConfiguration<ViolationType>
+    {
+        public void Configure(EntityTypeBuilder<ViolationType> builder)
+        {
+            builder.HasKey(vt => vt.ViolationTypeId);
+
+            // Properties
+            builder.Property(vt => vt.Description)
+                .HasMaxLength(500);
+
+            builder.Property(vt => vt.ViolationLevel)
+                .IsRequired()
+                .HasConversion<string>();
+
+            builder.Property(vt => vt.RequiredAmount)
+                .HasPrecision(10, 2);
+
+            builder.ToTable("ViolationTypes");
+        }
+    }
+}
