30e721b2097ddceabcac19c6594d91e5addd257f
Dev3amer <mohamedamer8921@gmail.com>
2025-04-25 06:46:10 +0300

    Create Role Seeder Class

---
+++ UniversityHousingSystem.Infrastructure/Seeding/RolesSeeder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MovieReservationSystem.Infrastructure.Seeding
+{
+    public static class RolesSeeder
+    {
+        public static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            var adminRole = new IdentityRole("Admin");
+            var employeeRole = new IdentityRole("Employee");
+            var userRole = new IdentityRole("User");
+
+            if (!roleManager.Roles.Any())
+            {
+                await roleManager.CreateAsync(adminRole);
+                await roleManager.CreateAsync(userRole);
+                await roleManager.CreateAsync(employeeRole);
+            }
+        }
+    }
+}
